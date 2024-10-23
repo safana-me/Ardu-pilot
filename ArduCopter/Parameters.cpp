MattKear/ardupilot

Copter: Autorotation mode restructure and tidy

ArduCopter/Parameters.cpp
@@ -1273,7 +1273,7 @@ ParametersG2::ParametersG2(void)
     ,mode_systemid_ptr(&copter.mode_systemid)
 #endif
 #if MODE_AUTOROTATE_ENABLED
-    ,arot()
+    ,arot(copter.inertial_nav, copter.ahrs, copter.motors)
 #endif
 #if HAL_BUTTON_ENABLED
     ,button_ptr(&copter.button)

safana-me/Ardu-pilot

ArduCopter/Parameters.cpp
@@ -1273,7 +1273,7 @@ ParametersG2::ParametersG2(void)
     ,mode_systemid_ptr(&copter.mode_systemid)
 #endif
 #if MODE_AUTOROTATE_ENABLED
-    ,arot()
+    ,arot(copter.ahrs, copter.motors, copter.attitude_control)
 #endif
 #if HAL_BUTTON_ENABLED
     ,button_ptr(&copter.button)