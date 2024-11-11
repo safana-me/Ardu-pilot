4980ee52a26baa195836c7f39a6493f0da047fd7
Peter Barker <pbarker@barker.dropbear.id.au>
2024-11-11 22:12:53 +1100

    AP_RangeFinder: move voltage_mv out of instance state
    Only one backend uses this state.
    
    Saves 2 bytes for each of the 10 instances.

--- libraries/AP_RangeFinder/AP_RangeFinder_Backend.h
+++ libraries/AP_RangeFinder/AP_RangeFinder_Backend.h
@@ -55,7 +55,7 @@ public:
     float distance() const { return state.distance_m; }
     uint16_t distance_cm() const { return state.distance_m*100.0f; }
     int8_t signal_quality_pct() const  WARN_IF_UNUSED { return state.signal_quality_pct; }
-    uint16_t voltage_mv() const { return state.voltage_mv; }
+    virtual uint16_t voltage_mv() const { return 0; }
     virtual int16_t max_distance_cm() const { return params.max_distance_cm; }
     virtual int16_t min_distance_cm() const { return params.min_distance_cm; }
     int16_t ground_clearance_cm() const { return params.ground_clearance_cm; }
